87a24a52d0dd117382bf88a1d3d1cf7ca260039f
jared <38332939+jaredaritter@users.noreply.github.com>
2020-06-30 14:37:24 -0400

    building dynamic nav bar

--- src/index.js
+++ src/index.js
@@ -1,1 +1,49 @@
-console.log('working?');
+// IMPORTS
+
+const pages = ['index', 'library', 'tic-tac-toe', 'restaurant'];
+
+function component(text) {
+  const element = document.createElement('div');
+  element.textContent = text;
+  document.body.appendChild(element);
+}
+
+const title = document.querySelector('title');
+if (title.textContent === 'Home') {
+  console.log(title);
+  component('Home');
+} else if (title.textContent === 'Library') {
+  console.log(title);
+  component('Library');
+}
+
+function buildNavBar() {
+  const nav = document.createElement('div');
+  nav.classList.add('nav');
+  pages.forEach((page) => {
+    const element = document.createElement('a');
+    element.href = `${page}.html`;
+    element.textContent = capitalize(page);
+    element.classList.add('navLink');
+    nav.appendChild(element);
+  });
+  console.log(nav);
+  return nav;
+}
+
+function capitalize(word) {
+  const newWord = word.charAt(0).toUpperCase() + word.slice(1);
+  return newWord;
+}
+
+const Dom = (() => {
+  const render = (...args) =>
+    args.forEach((arg) => {
+      document.body.appendChild(arg);
+    });
+  return {
+    render,
+  };
+})();
+
+Dom.render(buildNavBar());
