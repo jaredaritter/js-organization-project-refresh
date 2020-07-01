adc2729a0e5c22aad31a4f179169aacbe430b1f4
jared <38332939+jaredaritter@users.noreply.github.com>
2020-07-01 13:24:35 -0400

    Add class to table

--- src/dom.js
+++ src/dom.js
@@ -8,6 +8,7 @@ const Dom = (() => {
     content.forEach((para) => {
       const p = document.createElement('p');
       p.textContent = para;
+      p.classList.add('para');
       document.body.appendChild(p);
     });
   };
@@ -28,15 +29,18 @@ const Dom = (() => {
 
     // BUILD ATTACH THEAD TO TABLE
     author.textContent = 'Author';
-    tr.appendChild(author);
     title.textContent = 'Title';
-    tr.appendChild(title);
     pages.textContent = 'Pages';
-    tr.appendChild(pages);
     read.textContent = 'Read';
+    tr.appendChild(author);
+    tr.appendChild(title);
+    tr.appendChild(pages);
     tr.appendChild(read);
     thead.appendChild(tr);
     table.appendChild(thead);
+
+    // CLASS ASSIGNMENT
+    table.classList.add('table');
 
     // BUILD AND ATTACH TBODY TO TABLE
     const tbody = document.createElement('tbody');
